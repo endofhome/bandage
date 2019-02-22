7547de7bf3097449aa99e5cc2d840692032e6b47
Tom Barnsbury <ruralmyth@gmail.com>
2019-02-22 11:10:04 +0000

    Spin up HTTP4k application 'in container' (using Jetty)

--- src/main/kotlin/Bandage.kt
+++ src/main/kotlin/Bandage.kt
@@ -1,6 +1,15 @@
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
+
+fun main(args: Array<String>) {
+   val port = if (args.isNotEmpty()) args[0].toInt() else 7000
+   Bandage.asServer(Jetty(port)).start()
+
+   println("Bandage has started on port $port")
+}
 
 object Bandage: (Request) -> Response {
    override fun invoke(request: Request): Response = Response(OK)
