0c8d21d848ad416d922f9eda99392ce814eff52d
Tom Barnsbury <ruralmyth@gmail.com>
2019-02-26 18:20:22 +0000

    return empty login page

--- src/main/kotlin/Bandage.kt
+++ src/main/kotlin/Bandage.kt
@@ -1,16 +1,32 @@
+import org.http4k.core.Body
+import org.http4k.core.ContentType
 import org.http4k.core.Request
 import org.http4k.core.Response
+import org.http4k.core.with
 import org.http4k.core.Status.Companion.OK
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
+import org.http4k.template.HandlebarsTemplates
+import org.http4k.template.ViewModel
+import org.http4k.template.view
 
 fun main(args: Array<String>) {
-   val port = if (args.isNotEmpty()) args[0].toInt() else 7000
-   Bandage.asServer(Jetty(port)).start()
+    val port = if (args.isNotEmpty()) args[0].toInt() else Bandage.defaultPort
+    Bandage.asServer(Jetty(port)).start()
 
-   println("Bandage has started on port $port")
+    println("Bandage has started on port $port")
 }
 
-object Bandage: (Request) -> Response {
-   override fun invoke(request: Request): Response = Response(OK)
+object Bandage : (Request) -> Response {
+    const val defaultPort = 7000
+    private val renderer = HandlebarsTemplates().HotReload("src/main/resources")
+    private val view = Body.view(renderer, ContentType.TEXT_HTML)
+
+    override fun invoke(request: Request): Response {
+        return Response(OK).with(view of LoginPage)
+    }
+
+    object LoginPage : ViewModel {
+        override fun template() = "login-page"
+    }
 }
--- src/test/kotlin/BandageTest.kt
+++ src/test/kotlin/BandageTest.kt
@@ -3,12 +3,18 @@ import com.natpryce.hamkrest.equalTo
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Status.Companion.OK
+import org.http4k.webdriver.Http4kWebDriver
 import org.junit.jupiter.api.Test
 
 class BandageTest {
+
     @Test
-    fun `returns 200 OK for all requests`() {
-        val incomingRequest = Request(GET, "http://www.example.com")
-        assertThat(Bandage(incomingRequest).status, equalTo(OK))
+    fun `empty login page is returned`() {
+        val driver = Http4kWebDriver(Bandage)
+
+        driver.navigate().to("Bandage")
+
+        assertThat(driver.status, equalTo(OK))
+        assertThat(driver.title, equalTo("Bandage: Please log in"))
     }
 }
