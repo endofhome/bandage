10aa7c12e0d289cbe51ff9aefb05eb22d0831c35
Tom Barnsbury <ruralmyth@gmail.com>
2019-03-01 07:59:38 +0000

    add login form, hard-coded user list for now and no endpoint to handle login

--- src/test/kotlin/BandageTest.kt
+++ src/test/kotlin/BandageTest.kt
@@ -1,20 +1,29 @@
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.http4k.core.Method.GET
-import org.http4k.core.Request
 import org.http4k.core.Status.Companion.OK
 import org.http4k.webdriver.Http4kWebDriver
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import org.openqa.selenium.By
 
 class BandageTest {
 
     @Test
     fun `empty login page is returned`() {
-        val driver = Http4kWebDriver(Bandage)
+        val driver = Http4kWebDriver(Bandage.routes)
 
-        driver.navigate().to("Bandage")
+        driver.navigate().to("/")
 
         assertThat(driver.status, equalTo(OK))
         assertThat(driver.title, equalTo("Bandage: Please log in"))
+
+        val usernameField = driver.findElement(By.cssSelector("#user"))
+        assertNotNull(usernameField)
+
+        val passwordField = driver.findElement(By.cssSelector("#password"))
+        assertNotNull(passwordField)
+
+        val submitButton = driver.findElement(By.cssSelector("button[type=\"submit\"][name=\"login\"]"))
+        assertNotNull(submitButton)
     }
 }
