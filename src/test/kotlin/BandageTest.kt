f42dd9eaeb06a257eb0cd58469733a4bec01297e
Tom Barnsbury <ruralmyth@gmail.com>
2019-03-03 19:46:57 +0000

    Fully functional login page is returned

--- src/test/kotlin/BandageTest.kt
+++ src/test/kotlin/BandageTest.kt
@@ -1,30 +1,16 @@
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.http4k.core.Body
-import org.http4k.core.ContentType
-import org.http4k.core.Method
-import org.http4k.core.Method.POST
 import org.http4k.core.Status.Companion.OK
-import org.http4k.core.Request
-import org.http4k.core.Status.Companion.FOUND
-import org.http4k.core.Status.Companion.SEE_OTHER
-import org.http4k.core.body.Form
-import org.http4k.core.body.form
-import org.http4k.core.cookie.Cookie
-import org.http4k.core.cookie.cookies
-import org.http4k.core.with
-import org.http4k.lens.Header
-import org.http4k.lens.webForm
 import org.http4k.webdriver.Http4kWebDriver
-import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.By
-import java.util.*
+import org.openqa.selenium.Cookie
 
 class BandageTest {
 
     @Test
-    fun `empty login page is returned`() {
+    fun `fully functional login page is returned`() {
         val driver = Http4kWebDriver(Bandage.routes)
 
         driver.navigate().to("/")
@@ -32,13 +18,20 @@ class BandageTest {
         assertThat(driver.status, equalTo(OK))
         assertThat(driver.title, equalTo("Bandage: Please log in"))
 
-        val usernameField = driver.findElement(By.cssSelector("#user"))
-        assertNotNull(usernameField)
+        val usernameField = driver.findElement(By.cssSelector("#user")) ?: fail("username field not found")
+        val option = usernameField.findElement(By.cssSelector("option:contains(TB)"))
+        option.click()
 
-        val passwordField = driver.findElement(By.cssSelector("#password"))
-        assertNotNull(passwordField)
+        val passwordField = driver.findElement(By.cssSelector("#password")) ?: fail("password field not found")
+        passwordField.sendKeys(System.getenv("BANDAGE_PASSWORD"))
 
-        val submitButton = driver.findElement(By.cssSelector("button[type=\"submit\"][name=\"login\"]"))
-        assertNotNull(submitButton)
+        val loginButton = driver.findElement(By.cssSelector("button[type=\"submit\"][name=\"login\"]")) ?: fail("login button not found")
+        loginButton.click()
+
+        val loginCookie = driver.manage().getCookieNamed("bandage_login") ?: fail("login cookie not set")
+        val expectedCookie = Cookie("bandage_login", "${System.getenv("BANDAGE_API_KEY")}_3", "login")
+
+        assertThat(loginCookie, equalTo(expectedCookie))
+        assertThat(driver.currentUrl, equalTo("/dashboard"))
     }
 }
