1be2754ef33fbd762d0f5d30691d6cd3a3de5d59
Tom Barnsbury <ruralmyth@gmail.com>
2019-07-02 12:56:41 +0100

    EditMetadata returns 400 BAD REQUEST for a request with missing title field in form

--- src/test/kotlin/handlers/EditMetadataTest.kt
+++ src/test/kotlin/handlers/EditMetadataTest.kt
@@ -8,9 +8,11 @@ import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Status.Companion.BAD_REQUEST
 import org.http4k.core.UriTemplate
+import org.http4k.core.body.form
 import org.http4k.routing.RoutedRequest
 import org.junit.jupiter.api.Test
 import storage.DummyMetadataStorage
+import java.util.UUID
 
 class EditMetadataTest {
     @Test
@@ -31,6 +33,21 @@ class EditMetadataTest {
         val authenticatedRequest = AuthenticatedRequest(
             RoutedRequest(
                 Request(GET, "/tracks/some_id"),
+                UriTemplate.from("/tracks/{id}")
+            ),
+            exampleUser
+        )
+
+        assertThat(EditMetadata(authenticatedRequest, DummyMetadataStorage()).status, equalTo(BAD_REQUEST))
+    }
+
+    @Test
+    fun `missing title field in form returns 400 BAD REQUEST`() {
+        val authenticatedRequest = AuthenticatedRequest(
+            RoutedRequest(
+                Request(GET, "/tracks/${UUID.randomUUID()}").form(
+                    "not_title", "some value"
+                ),
                 UriTemplate.from("/tracks/{id}")
             ),
             exampleUser
