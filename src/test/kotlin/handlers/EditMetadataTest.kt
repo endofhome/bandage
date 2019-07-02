99bc340fa3d7969588c3eadf3b696c73ab5a0c08
Tom Barnsbury <ruralmyth@gmail.com>
2019-07-02 12:44:43 +0100

    EditMetadata returns 400 BAD REQUEST for a request with no id path parameter

---
+++ src/test/kotlin/handlers/EditMetadataTest.kt
@@ -0,0 +1,28 @@
+package handlers
+
+import AuthenticatedRequest
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import exampleUser
+import org.http4k.core.Method.GET
+import org.http4k.core.Request
+import org.http4k.core.Status.Companion.BAD_REQUEST
+import org.http4k.core.UriTemplate
+import org.http4k.routing.RoutedRequest
+import org.junit.jupiter.api.Test
+import storage.DummyMetadataStorage
+
+class EditMetadataTest {
+    @Test
+    fun `no id path parameter returns 400 BAD REQUEST`() {
+        val authenticatedRequest = AuthenticatedRequest(
+            RoutedRequest(
+                Request(GET, "/tracks/some_id"),
+                UriTemplate.from("/tracks/{id}")
+            ),
+            exampleUser
+        )
+
+        assertThat(EditMetadata(authenticatedRequest, DummyMetadataStorage()).status, equalTo(BAD_REQUEST))
+    }
+}
