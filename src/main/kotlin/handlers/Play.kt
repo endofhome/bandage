d1e1219a7308914de2b9f2d4ba01dae332330d6a
Tom Barnsbury <ruralmyth@gmail.com>
2019-09-16 22:17:25 +0100

    spike adding metadata to files when streaming - test using a FIFO file to see how this behaves

--- src/main/kotlin/handlers/Play.kt
+++ src/main/kotlin/handlers/Play.kt
@@ -16,11 +16,19 @@ import result.Result.Failure
 import result.Result.Success
 import result.map
 import result.orElse
+import storage.AudioTrackMetadata
 import storage.AudioTrackMetadataEnhancer
 import storage.FileStorage
 import storage.MetadataStorage
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
 import java.time.format.DateTimeFormatter
 import java.util.UUID
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
+
 
 object Play {
     operator fun invoke(
@@ -31,11 +39,13 @@ object Play {
         request.query("id")?.let { return Response(SEE_OTHER).header("Location", "$play/$it") }
 
         val uuid = request.path("id") ?: return Response(BAD_REQUEST)
-        val metadata = metadataStorage.findTrack(UUID.fromString(uuid)).map { it }.orElse { null } ?: return Response(NOT_FOUND)
-        val enhancedMetadata = when (val enhanced = with(AudioTrackMetadataEnhancer) { metadata.enhanceWithTakeNumber(metadataStorage) }) {
-            is Success -> enhanced.value
-            is Failure -> return Response(INTERNAL_SERVER_ERROR)
-        }
+        val metadata =
+            metadataStorage.findTrack(UUID.fromString(uuid)).map { it }.orElse { null } ?: return Response(NOT_FOUND)
+        val enhancedMetadata =
+            when (val enhanced = with(AudioTrackMetadataEnhancer) { metadata.enhanceWithTakeNumber(metadataStorage) }) {
+                is Success -> enhanced.value
+                is Failure -> return Response(INTERNAL_SERVER_ERROR)
+            }
 
         return fileStorage.stream(metadata.passwordProtectedLink).map { audioStream ->
             val dateTimePattern = DateTimeFormatter.ofPattern(
@@ -48,13 +58,74 @@ object Play {
                 "Content-Length" to metadata.fileSize.toString(),
                 "Content-Range" to "bytes 0-${metadata.fileSize - 1}/${metadata.fileSize}",
                 "content-disposition" to "attachment; filename=${
-                    listOf(dateTime, "$title${enhancedMetadata.takeNumber?.let { " (take $it)" }.orEmpty()}").joinToString(" ")
+                listOf(
+                    dateTime,
+                    "$title${enhancedMetadata.takeNumber?.let { " (take $it)" }.orEmpty()}"
+                ).joinToString(" ")
                 }.${metadata.format}"
             )
-            Response(OK).body(audioStream).headers(headers)
+
+            // TODO stream should be closed.
+            // TODO also - the downloader that provides the inputstream should also be closed.
+            Response(OK).body(audioStream.newMp3Headers(metadata)).headers(headers)
         }.orElse {
             logger.warn(it.message)
             Response(NOT_FOUND)
         }
     }
+
+    private fun InputStream.newMp3Headers(metadata: AudioTrackMetadata): InputStream {
+        val tempDir = File("/tmp/bandage")
+        if (! tempDir.exists()) tempDir.mkdir()
+        val fifoPath = "${tempDir.absolutePath}/${metadata.uuid}"
+        val mkFifo = listOf("mkfifo", fifoPath)
+        ProcessBuilder().command(mkFifo).start().waitFor()
+        val fifoFile = File(fifoPath)
+
+        writeStreamToFile(fifoFile, metadata)
+
+        val ffmpegNewMetadata = listOf(
+            ffmpegForCurrentOs(),
+            "-i", fifoFile.absolutePath,
+            "-metadata", "artist=${metadata.artist}",
+            "-metadata", "title=${metadata.preferredTitle().first}",
+            "-acodec", "copy",
+            "-f", metadata.format, "-"
+        )
+
+        val process = ProcessBuilder().command(ffmpegNewMetadata).start()
+
+        return process.inputStream
+    }
+
+    private fun InputStream.writeStreamToFile(fifoFile: File, metadata: AudioTrackMetadata) {
+        val ogThead = Thread.currentThread()
+        val backgroundThread: Thread = thread(start = true, name = "write-${metadata.hash}") {
+            this.use { input ->
+                FileOutputStream(fifoFile).use { output ->
+                    input.copyTo(output)
+                }
+            }
+            ogThead.run { println("inputstream and outputstreams are both closed") } // TODO temp logging
+        }
+
+        // TODO temp logging
+        var tripswitch = 1
+        val copyingFile = Runnable {
+            if (backgroundThread.isAlive) println("${backgroundThread.name} - copying file")
+            else if (tripswitch == 1) {
+                println("${backgroundThread.name} - copying complete.")
+                tripswitch = 0
+            }
+        }
+        val executor = Executors.newScheduledThreadPool(1)
+        executor.scheduleAtFixedRate(copyingFile, 0, 1, TimeUnit.SECONDS)
+    }
+
+    private fun ffmpegForCurrentOs(): String =
+        if (System.getProperty("os.name").toLowerCase().startsWith("mac")) {
+            "lib/ffmpeg_darwin"
+        } else {
+            "lib/ffmpeg_linux_x64"
+        }
 }
