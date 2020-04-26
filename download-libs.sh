78c8ca87f16f314671f59bd57299ed169b745150
Tom Barnsbury <ruralmyth@gmail.com>
2020-04-26 21:46:38 +0100

    Fix test for OS type in download-libs.sh

--- download-libs.sh
+++ download-libs.sh
@@ -9,11 +9,10 @@ if [[ -z $curl_installed ]]; then
 fi
 
 if [[ -z $audiowaveform_installed ]]; then
-    os_type=$(echo $OSTYPE)
-    if [[ os_type -eq "darwin" ]];then
+    if [[ $OSTYPE == "darwin"* ]];then
       printf "* Downloading audiowaveform for macOS... "
       curl -Ls https://www.dropbox.com/s/ahqikw808knb5su/audiowaveform-darwin-1.4.1?raw=1 -o lib/audiowaveform
-    elif [[ os_type -eq "linux-gnu" ]]; then
+    elif [[ $OSTYPE == "linux-gnu" ]]; then
       printf "* Downloading audiowaveform for Linux... "
       curl -Ls https://www.dropbox.com/s/8ezc6k1fbg74n52/audiowaveform-linux-x64-1.4.1?raw=1 -o lib/audiowaveform
     else
