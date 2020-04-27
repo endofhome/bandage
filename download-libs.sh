e51a3d55e7b03f3596a31c26485eb7319094695c
Tom Barnsbury <ruralmyth@gmail.com>
2020-04-27 17:24:56 +0100

    Download ffmpeg and ffprobe during build if not available locally.
    I want to get these out of version control, however there are few things that aren't that nice here.
    
    Firstly, the files are large - approx 70 MB each. This means the build is significantly slower. If a Docker image could be cached on CI this might be a faster option (though if the image has to be downloaded every time it might be slower).
    
    Secondly, there's lots of duplication in this script that could be factored out.

--- download-libs.sh
+++ download-libs.sh
@@ -2,6 +2,8 @@
 
 curl_installed=$(command -v curl)
 audiowaveform_installed=$(command -v lib/audiowaveform)
+ffmpeg_installed=$(command -v lib/ffmpeg)
+ffprobe_installed=$(command -v lib/ffprobe)
 
 if [[ -z $curl_installed ]]; then
   echo "Error: cURL is not installed."
@@ -29,4 +31,50 @@ if [[ -z $audiowaveform_installed ]]; then
       chmod +x lib/audiowaveform
 else
     echo "* Local audiowaveform already installed"
+fi
+
+if [[ -z $ffmpeg_installed ]]; then
+    if [[ $OSTYPE == "darwin"* ]];then
+      printf "* Downloading ffmpeg for macOS... "
+      curl -Ls https://www.dropbox.com/s/zdby9dzm0r5jrhu/ffmpeg_darwin_4.2.2?raw=1 -o lib/ffmpeg
+    elif [[ $OSTYPE == "linux-gnu" ]]; then
+      printf "* Downloading ffmpeg for Linux... "
+      curl -Ls https://www.dropbox.com/s/w1agd8evvbuz0d5/ffmpeg_linux_x64?raw=1 -o lib/ffmpeg
+    else
+      echo
+      echo "Error: your OS $os_type is unsupported."
+      exit 1
+    fi
+    if [[ $? -eq 0 ]]; then
+        echo "OK"
+      else
+        echo "Error downloading"
+      exit 1
+      fi
+      chmod +x lib/ffmpeg
+else
+    echo "* Local ffmpeg already installed"
+fi
+
+if [[ -z $ffprobe_installed ]]; then
+    if [[ $OSTYPE == "darwin"* ]];then
+      printf "* Downloading ffprobe for macOS... "
+      curl -Ls https://www.dropbox.com/s/ghla06caoahj5lu/ffprobe_darwin_4.2.2?raw=1 -o lib/ffprobe
+    elif [[ $OSTYPE == "linux-gnu" ]]; then
+      printf "* Downloading ffprobe for Linux... "
+      curl -Ls https://www.dropbox.com/s/5fge9t5pmsf4gd4/ffprobe_linux_x64?raw=1 -o lib/ffprobe
+    else
+      echo
+      echo "Error: your OS $os_type is unsupported."
+      exit 1
+    fi
+    if [[ $? -eq 0 ]]; then
+        echo "OK"
+      else
+        echo "Error downloading"
+      exit 1
+      fi
+      chmod +x lib/ffprobe
+else
+    echo "* Local ffprobe already installed"
 fi
