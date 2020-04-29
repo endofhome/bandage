ddd1c1b746a052a773f86ee5577f8401897d0cf7
Tom Barnsbury <ruralmyth@gmail.com>
2020-04-29 22:32:06 +0100

    Refactoring - remove duplication in test

--- src/test/kotlin/unit/tests/WaveformNormalisationTests.kt
+++ src/test/kotlin/unit/tests/WaveformNormalisationTests.kt
@@ -3,20 +3,21 @@ package unit.tests
 import Audiowaveform
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import json
+import json.json
 import normalise
 import org.junit.jupiter.api.Test
 import java.io.File
 
 internal class WaveformNormalisationTests {
+    private val baseDirectory = "src/test/resources/waveform/"
 
     @Test
     fun `Waveform is correctly normalised`() {
-        val exampleInputFile = File("src/test/resources/waveform/audiowaveform-input.json")
+        val exampleInputFile = File(baseDirectory + "audiowaveform-input.json")
         val exampleInputString = exampleInputFile.readLines().single()
         val exampleWaveform = json.parse(Audiowaveform.serializer(), exampleInputString)
 
-        val expectedFile = File("src/test/resources/waveform/approved-audiowaveform.json")
+        val expectedFile = File(baseDirectory + "audiowaveform-input.approved")
         val expectedString = expectedFile.readLines().single()
         val expectedData = json.parse(Audiowaveform.serializer(), expectedString).data
 
